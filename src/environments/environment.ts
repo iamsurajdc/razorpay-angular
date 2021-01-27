4678371149bd4464be7eacc92fad3adccacedff7
Suraj Chandgude <surajchandgude0304@gmail.com>
2021-01-27 11:32:17 +0530

    Updated details

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,14 +8,14 @@ export const environment = {
     createOrder: 'YOUR_CREATE_ORDER_CLOUD_FUNCTION_URL',
     capturePayment: 'YOUR_CAPTURE_PAYMENT_CLOUD_FUNCTION_URL'
   },
-  RAZORPAY_KEY_ID: 'rzp_test_0wzrsNDtOxDPKP'
+  RAZORPAY_KEY_ID: 'RAZORPAY_KEY_ID_VALUE'
 };
 
 
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
+ *rzp_test_0wzrsNDtOxDPKP
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
